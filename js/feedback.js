1b69e1bcb30bffc9f413a9a4007deee211c18089
Rohith <rohithkanna.ss@gmail.com>
2026-03-25 14:09:32 +0530

    Feedback section IV

--- js/feedback.js
+++ js/feedback.js
@@ -185,12 +185,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
       fetch(APPS_SCRIPT_URL, {
         method: 'POST',
+        mode: 'no-cors',
         headers: {
           'Content-Type': 'text/plain;charset=utf-8',
         },
         body: JSON.stringify(payload),
       })
-      .then(res => res.text())
       .then(() => handleSuccess())
       .catch(() => handleError());
     });
